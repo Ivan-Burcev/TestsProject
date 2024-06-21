86283efce9ad0dee5a1a7adbce236312866aa091
IVAN_BURCEV <amrdw.2226.ru@gmail.com>
2024-06-21 09:30:47 +0300

    Simple commit

---
+++ TestJavaDocker/src/test/java/org/example/SimpleTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import org.testng.annotations.Test;
+
+
+public class SimpleTest {
+    @Test
+    public void testCase () {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
